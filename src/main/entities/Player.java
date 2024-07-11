5bf4b0a2c9fa85783f91fac615ac4891e717d06e
Gabriele Losi <71230811+LosiG@users.noreply.github.com>
2024-07-11 15:26:15 +0200

    20-xp-and-stats (#21)
    * Added experience to the game
    
    * Added stats dexterity e strength al player and method lvl up
    
    * Added scaling based on stats on projectile damage and player speed
    
    * Basic implementation of xp + update on collision logic
    
    * make collision test readable

--- src/main/entities/Player.java
+++ src/main/entities/Player.java
@@ -1,5 +1,6 @@
 package main.entities;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
@@ -9,7 +10,13 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 
+import main.ui.GamePanel;
+
 public class Player extends Entity {
+    public Integer exp;
+    public Integer lvl;
+    public Integer strength;
+    public Integer dexterity;
 
     public Player(Integer currentX, Integer currentY, Integer spriteY, Integer spriteX, Integer speed,
             Integer acceleration, int hp) {
@@ -21,6 +28,11 @@ public class Player extends Entity {
         this.acceleration = acceleration;
         this.currentHp = this.maxHp = hp;
         this.invincibility = 1;
+        this.damage = 1;
+        this.exp = 0;
+        this.lvl = 1;
+        this.strength = 10;
+        this.dexterity = 10;
         try {
             String playerImage = "assets/player.png";
             img = ImageIO.read(
@@ -71,15 +83,34 @@ public class Player extends Entity {
 
     @Override
     public void paint(Graphics2D graphic) {
-
+        paintXpBar(this, graphic);
         paintHpBar(this, graphic);
-
         // Draw the body
         if (img != null) {
             graphic.drawImage(img, this.currentX, this.currentY, null);
         } else {
             graphic.fillRect(this.currentX, this.currentY, this.spriteX, this.spriteY);
         }
+    }
 
+    private void paintXpBar(Player player, Graphics2D graphic) {
+        float xpRatio = (float) player.exp / 100;
+        Integer xpLenght = Math.round(800 * xpRatio);
+
+        graphic.setColor(Color.white);
+        graphic.fillRect(0, 0, 800, 6);
+
+        graphic.setColor(Color.green);
+        graphic.fillRect(0, 0, xpLenght, 6);
+
+    }
+
+    public void levelUp() {
+        this.maxHp += 3;
+        this.currentHp = maxHp;
+        this.strength += 1;
+        this.dexterity += 1;
+        this.lvl += 1;
+        this.exp = 0;
     }
 }
